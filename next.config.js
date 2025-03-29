772cc8dd55ffb2edb212674864dfe849fadc19c5
SravyaKavuri491 <ksravya410@gmail.com>
2025-03-29 11:18:23 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Use your existing .eslintrc file but don't fail build
+    ignoreDuringBuilds: process.env.NODE_ENV === 'production', // Only ignore in production
+  },
 }
 
 module.exports = nextConfig
