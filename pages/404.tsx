cb1ea96f56fc4b1bdb3bcdcf66699cc0740f3ee6
SravyaKavuri491 <ksravya410@gmail.com>
2025-03-29 11:35:30 -0500

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -10,12 +10,27 @@ export default function ErrorPage() {
       display="flex"
       alignItems="center"
       justifyContent="center"
+      p={4}
     >
-      <Box bg="white" p={8} borderRadius="lg" boxShadow="lg" maxW="md" w="full">
-        <Heading as="h1" size="xl" mb={4}>
+      <Box 
+        bg="white" 
+        p={8} 
+        borderRadius="lg" 
+        boxShadow="lg" 
+        maxW="md" 
+        w="full"
+      >
+        <Heading 
+          as="h1" 
+          size="xl" 
+          mb={4}
+          sx={{ '&': { color: 'gray.800' } }}
+        >
           404 - Page Not Found
         </Heading>
-        <Text>Oops! The page you are looking for doesn&apos;t exist.</Text>
+        <Text color="gray.600">
+          Oops! The page you are looking for doesn&apos;t exist.
+        </Text>
       </Box>
     </Box>
   );
